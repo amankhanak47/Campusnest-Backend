034adc065da211f99637ae4fcc1d75b0f8baeea9
Yashwanth Reddy <yashwanthreddy072@gmail.com>
2024-03-11 17:25:29 +0530

    Review

--- modals/Reviews.js
+++ modals/Reviews.js
@@ -37,7 +37,7 @@ const Reviews = sequelize.define(
       type: DataTypes.INTEGER,
     },
     feedback: {
-      type: DataTypes.TEXT,
+      type: DataTypes.STRING,
     },
     role: {
       type: DataTypes.STRING,
